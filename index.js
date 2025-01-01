86ded9fcd33b7573a282e53014bffdee3c1a8b01
SanatShourya <sanatshourya100@gmail.com>
2024-12-31 21:50:26 -0500

    completed project, made completely responsive and added all functionalities

---
+++ index.js
@@ -0,0 +1,15 @@
+const icon1 = document.getElementsByClassName('nav__hamburger')[0];
+const icon2 = document.getElementsByClassName('nav__cross')[0];
+const hiddenDiv = document.getElementsByClassName('nav__collapsediv')[0];
+
+// Add onclick functionality to the icon
+icon1.onclick = function myFunctionOpen() {
+    hiddenDiv.classList.add('show__div'); // Show the div
+    icon2.classList.add('show__cross');
+};
+
+// delete onclick functionality to the icon
+icon2.onclick = function myFunctionClose() {
+    hiddenDiv.classList.remove('show__div'); // Remove the div
+    icon2.classList.remove('show__cross');
+};
