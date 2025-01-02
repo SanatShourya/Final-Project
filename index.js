c0825068b28f9a5572bdb895fb61211dba2922b4
SanatShourya <sanatshourya100@gmail.com>
2025-01-01 21:53:44 -0500

    minor changes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const icon1 = document.getElementsByClassName('nav__hamburger')[0];
 const icon2 = document.getElementsByClassName('nav__cross')[0];
 const hiddenDiv = document.getElementsByClassName('nav__collapsediv')[0];
 
-// Add onclick functionality to the icon
+// Add onclick functionality to the icon1
 icon1.onclick = function myFunctionOpen() {
     hiddenDiv.classList.add('show__div'); // Show the div
-    icon2.classList.add('show__cross');
+    icon2.classList.add('show__cross'); // Show the cross
 };
 
-// delete onclick functionality to the icon
+// delete onclick functionality to the icon2
 icon2.onclick = function myFunctionClose() {
     hiddenDiv.classList.remove('show__div'); // Remove the div
-    icon2.classList.remove('show__cross');
+    icon2.classList.remove('show__cross'); // Remove the cross
 };
